bd76de2fd56777bf39be9aefb045738d3673f5a1
Jofre Jaime <jofredenovais@gmail.com>
2023-02-21 19:01:57 +0100

    Create a project added

--- src/Components/User/UserCreateAProject.js
+++ src/Components/User/UserCreateAProject.js
@@ -27,7 +27,7 @@ function UserCreateAProject() {
   const [img, setImg] = React.useState({});
   const [vd, setVid] = React.useState({});
   const [aud, setAud] = React.useState({});
-  const mobile =  useMedia("(max-width: 670px)");
+  const mobile = useMedia("(max-width: 670px)");
   const [prev, setPrev] = useState(false);
 
   function handleImg({ target }) {
@@ -36,85 +36,101 @@ function UserCreateAProject() {
       raw: target.files[0],
     });
   }
-  
+
   function handleAud({ target }) {
     setAud({
       preview: URL.createObjectURL(target.files[0]),
       raw: target.files[0],
     });
   }
-  
+
   function handleVid({ target }) {
     setVid({
       preview: URL.createObjectURL(target.files[0]),
       raw: target.files[0],
     });
   }
-  
+
   function handleSubmit(event) {
     event.preventDefault();
   }
-  
-    
-  return (<>
-      {mobile ? <div className={styles.createHeader}> 
-      <button className={styles.btnHeader} onClick={()=>setPrev(false)}>Form</button> <button onClick={()=>setPrev(true)} className={styles.btnHeader}>preview</button></div> : '' }
-      
-    <section className={`${ mobile? styles.createMobile:''} ${styles.create} animeLeft`}>
-      
-      <form onSubmit={handleSubmit}>
-        <Input label={"Titulo do projeto"} type="text" name={"titulo"}  />
-       
-        <TextArea label={"Descrição do projecto"} name={"desc"} />
-        <Input
-          label={"Adicionar Imagem [recomendado]"}
-          type={"file"}
-          id="img"
-          name={"imgage"}
-          onChange={handleImg}
-        />
-        <Input
-          label={"Adicionar Video"}
-          type="file"
-          id="video"
-          name="video"
-          onChange={handleVid}
-        />
-        <Input
-          label={"Adicionar Audio"}
-          type="file"
-          id="audio"
-          name="audio"
-          onChange={handleAud}
-        />
-        <Input
-          label={"GitHub [opcional]"}
-          placeholder="ex: htttps://www.github.com/username/repository"
-        />
-        <Select
-          label={"Area de desenvolvimento"}
-          options={areas}
-          setValue={setArea}
-        />
-        <Select
-          label={"Linguagens usadas"}
-          options={techs}
-          setValue={setTechs}
-        />
-        <Select
-          label={"Ferramenta usadas [opcional]"}
-          options={ferramentas}
-          setValue={setFerramenta}
-        />
-     
-      </form>   
-      {(img.preview || prev) && (
+
+  return (
+    <>
+      {mobile ? (
+        <div className={styles.createHeader}>
+          <button className={styles.btnHeader} onClick={() => setPrev(false)}>
+            Form
+          </button>{" "}
+          <button onClick={() => setPrev(true)} className={styles.btnHeader}>
+            preview
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
+
+      <section
+        className={`${mobile ? styles.createMobile : ""} ${
+          styles.create
+        } animeLeft`}
+      >
+        <form onSubmit={handleSubmit}>
+          <Input label={"Titulo do projeto"} type="text" name={"titulo"} />
+
+          <TextArea label={"Descrição do projecto"} name={"desc"} />
+          <Input
+            label={"Adicionar Imagem [recomendado]"}
+            type={"file"}
+            id="img"
+            name={"imgage"}
+            onChange={handleImg}
+          />
+          <Input
+            label={"Adicionar Video"}
+            type="file"
+            id="video"
+            name="video"
+            onChange={handleVid}
+          />
+          <Input
+            label={"Adicionar Audio"}
+            type="file"
+            id="audio"
+            name="audio"
+            onChange={handleAud}
+          />
+          <Input
+            label={"GitHub [opcional]"}
+            placeholder="ex: htttps://www.github.com/username/repository"
+          />
+          <Select
+            label={"Area de desenvolvimento"}
+            options={areas}
+            setValue={setArea}
+          />
+          <Select
+            label={"Linguagens usadas"}
+            options={techs}
+            setValue={setTechs}
+          />
+          <Select
+            label={"Ferramenta usadas [opcional]"}
+            options={ferramentas}
+            setValue={setFerramenta}
+          />
+        </form>
+        {(img.preview || prev) && (
           <div
-            className={`${styles.preview} ${mobile ? styles.previewMobile:''} ${prev ? styles.prev : '' }`}
-            style={{ borderImage: `url('${img.preview}')`}}
-          > </div>
+            className={`${styles.preview} ${
+              mobile ? styles.previewMobile : ""
+            } ${prev ? styles.prev : ""}`}
+            style={{ borderImage: `url('${img.preview}')` }}
+          >
+            {" "}
+          </div>
         )}
-    </section>
+      </section>
     </>
   );
 }
